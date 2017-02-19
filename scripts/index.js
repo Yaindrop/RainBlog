1cbc79cd2a1e90d4c1703cfb98f4302146e43046
D Y <Raindrop@Ds-MacBook-Pro.local>
2017-02-19 23:38:58 +0800

    Update 2.19

--- scripts/index.js
+++ scripts/index.js
@@ -3,55 +3,115 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     "use strict";
     
+    /*ajax temp code*/
     document.getElementById("name").addEventListener("click", function () {
         $ajax.sendGetRequest("scripts/temp.json", function (request) {
             window.alert(request.name);
         }, true);
     });
     
-    var socialEmerged = false,
-        socialRunning = false;
+    /*Social Tag Animation*/
+    var socialRunning = false,
+        socialShown = false;
     document.getElementById("social").addEventListener("click", function () {
         if (!socialRunning) {
-            var tags = document.getElementsByClassName("social-tag"),
+            var tags = document.getElementsByClassName("social-tag");
+            socialRunning = true;
+            var num;
+            if (!socialShown) {
                 num = 0;
-            if (!socialEmerged) {
-                num = 0;
-                var timer_in = window.setInterval(function () {
-                    socialRunning = true;
-                    if (num < 3) {
-                        tags[num].style.display = "block";
-                        tags[num].style.animation = "emerge .15s ";
-                    }
-                    if (num > 0) {
-                        tags[num - 1].style.opacity = 1;
-                    }
-                    if (num === 3) {
-                        window.clearInterval(timer_in);
-                        socialRunning = false;
-                        socialEmerged = true;
-                    }
-                    num ++;
-                }, 50);
             } else {
-                num = 2;
-                var timer_out = window.setInterval(function () {
-                    socialRunning = true;
-                    if (num > -1) {
-                        tags[num].style.animation = "vanish .15s ";
-                    }
-                    if(num < 2){
-                        tags[num + 1].style.display = "none";
-                        tags[num + 1].style.opacity = 0;
-                    }
-                    if (num === -1) {
-                        window.clearInterval(timer_out);                       
-                        socialRunning = false;
-                        socialEmerged = false;
-                    }
+                num = tags.length - 1;
+            }
+            var timer = window.setInterval(function () {
+                tags[num].classList.toggle("social-tag-shown");
+                if (!socialShown) {
+                    num ++;
+                } else {
                     num --;
-                }, 50);
+                }
+                if ((socialShown && num == -1)||num === tags.length) {
+                    window.clearInterval(timer);
+                    socialShown = !socialShown;
+                    socialRunning = false;
+                }
+            }, 100);
+        }
+    });
+    
+    /*Back to Top*/
+    document.getElementById("back-to-top").addEventListener("click", smoothscroll);
+    function smoothscroll () {
+        var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+        if (currentScroll > 0) {
+            window.requestAnimationFrame(smoothscroll);
+            window.scrollTo (0,currentScroll - (currentScroll/4));
+        }
+    }
+    /*Music Player*/
+    /*Player Show & Hide*/
+    document.getElementById("player-button").addEventListener("click", function () {
+        var button = document.getElementById("player-button");
+        var control = document.getElementById("player-control");
+        var bar = document.getElementById("player-bar");
+        var num = 0,
+            timer = window.setInterval(function () {
+                if (num === 0) button.classList.toggle("player-button-hide")
+                if (num === 1) control.classList.toggle("player-shown");
+                if (num === 2) {
+                    bar.classList.toggle("player-shown");
+                    window.clearInterval(timer);
+                }
+                num ++;
+            }, 160);
+    });
+    document.getElementById("music-hide").addEventListener("click", function () {
+        var button = document.getElementById("player-button");
+        var control = document.getElementById("player-control");
+        var bar = document.getElementById("player-bar");
+        var button = document.getElementById("player-button");
+        var control = document.getElementById("player-control");
+        var bar = document.getElementById("player-bar");
+        var num = 0,
+            timer = window.setInterval(function () {
+                if (num === 0) bar.classList.toggle("player-shown");
+                if (num === 1) control.classList.toggle("player-shown");
+                if (num === 2) {
+                    button.classList.toggle("player-button-hide")
+                    window.clearInterval(timer);
+                }
+                num ++;
+            }, 160);
+    });
+    /*Music Button Show & Hide*/
+    var MBShowed = false;
+    function showMB () {
+        if(!MBShowed){
+            var buttons = document.getElementsByClassName("music-button");
+            for (var i = 0; i < buttons.length; i ++) {
+                buttons[i].classList.toggle("music-button-shown");
             }
         }
+        MBShowed = true;
+    }
+    function hideMB () {
+        if(MBShowed){
+            var buttons = document.getElementsByClassName("music-button");
+            for (var i = 0; i < buttons.length; i ++) {
+                buttons[i].classList.toggle("music-button-shown");
+            }
+        }
+        MBShowed = false;
+    }
+    document.getElementById("music-cover").addEventListener("mouseenter", showMB);
+    document.getElementById("player-control").addEventListener("mouseleave", hideMB);
+    document.getElementById("player-control").addEventListener("click", function () {
+        if(!MBShowed) showMB();
+    });
+    document.getElementById("content").addEventListener("click", function () {
+        if(MBShowed) hideMB();
+    });
+    document.getElementById("music-title").addEventListener("click", function () {
+        document.getElementById("music-title").classList.toggle("info-active");
     });
 });
